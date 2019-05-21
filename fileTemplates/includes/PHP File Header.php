43aeb91dbe4247091bf6f59d94355f777a44fe6a
Alexandre Balmes <alexandre@vanoix.com>
2019-05-21 13:37:11 +0200

    PS-2019.1.2 <vanoix@vanoix-dell Create baseRefactoring.xml, PHP Interface Doc Comment.php, PHP Class.php, ide.general.xml, project.default.xml, rainbow_brackets.xml, PHP Trait Doc Comment.php, ea_extended.xml, PHP Constructor.php, solhint.xml, PHP Class Doc Comment.php, terminal.xml, Default.xml, PHP Field Doc Comment.php, ignore.xml, code.style.schemes, find.xml, filetypes.xml, php.xml, colors.scheme.xml, PHPSpec Specification.php, PHP Trait.php, databaseDrivers.xml, PHP Getter Method.php, PHP Function Doc Comment.php, Default.xml, PHPUnit 6 Test.php, PHPUnit Test.php, PHP Fluent Setter Method.php, dayNightSettings.xml, laf.xml, PHP File Header.php, PHP Setter Method.php, PHP Interface.php, editor.xml, markdown_navigator.shared.xml, debugger.xml, a-file-icons.xml, PHP File.php, updates.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,8 @@
+/**
+ * This file is part of ${FILE_HEADER_PACKAGE}.
+ * 
+ * ${FILE_HEADER_COPYRIGHT}
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
